a5757957eead0d12a6d146924229060ce3c157cd
Katelyn McInerney <kamciner@unc.edu>
2024-11-25 10:48:04 -0500

    added another histogram: by tree health

--- scripts/plot_tree_size_histogram.R
+++ scripts/plot_tree_size_histogram.R
@@ -7,14 +7,28 @@ library(tidyverse)
 tree_data <- read_csv("./data/derived_data/tree_data_cleaned.csv")
 
 # plot histogram of tree sizes, colored by borough
-tree_histogram <- tree_data %>% 
+tree_histogram_borough <- tree_data %>% 
   ggplot(aes(x = tree_dbh, colour = boro_name)) + 
-  geom_freqpoly(binwidth = 1) + 
+  geom_freqpoly(binwidth = 1, linewidth = 1) + 
   labs(title = "Distribution of tree diameters by borough", 
-       x = "Tree diameter (inches)", y = "Count", colour = "borough") + 
+       x = "Tree diameter (inches)", y = "Count", colour = "Borough") + 
   theme_bw() + 
   theme(legend.position = "bottom")
 
 # save plot
-ggsave("./figures/tree_diameter_histogram.png", tree_histogram, 
+ggsave("./figures/tree_diameter_by_borough_histogram.png", tree_histogram_borough, 
+       width = 8, height = 6, dpi = 300)
+
+# plot histogram of tree sizes, colored by health
+tree_histogram_health <- tree_data %>% 
+  ggplot(aes(x = tree_dbh, colour = health)) + 
+  geom_freqpoly(binwidth = 1, linewidth = 1) + 
+  labs(title = "Distribution of tree diameters by health", 
+       x = "Tree diameter (inches)", y = "Count", colour = "Health") + 
+  scale_colour_discrete(type = c("red", "lightgreen", "forestgreen",  "orange")) +
+  theme_bw() + 
+  theme(legend.position = "bottom")
+
+# save plot
+ggsave("./figures/tree_diameter_by_health_histogram.png", tree_histogram_health, 
        width = 8, height = 6, dpi = 300)
