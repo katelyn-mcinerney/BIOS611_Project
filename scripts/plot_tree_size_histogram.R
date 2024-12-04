f83130454c005481e4c28cf46312b302156784b7
Katelyn McInerney <kamciner@unc.edu>
2024-12-04 18:10:04 -0500

    changed plot from freqpoly to density plot

--- scripts/plot_tree_size_histogram.R
+++ scripts/plot_tree_size_histogram.R
@@ -9,9 +9,9 @@ tree_data <- read_csv("./data/derived_data/tree_data_cleaned.csv")
 # plot histogram of tree sizes, colored by borough
 tree_histogram_borough <- tree_data %>% 
   ggplot(aes(x = tree_dbh, colour = boro_name)) + 
-  geom_freqpoly(binwidth = 1, linewidth = 1) + 
+  geom_density(linewidth = 1) + 
   labs(title = "Distribution of tree diameters by borough", 
-       x = "Tree diameter (inches)", y = "Count", colour = "Borough") + 
+       x = "Tree diameter (inches)", y = "Density", colour = "Borough") + 
   theme_bw() + 
   theme(legend.position = "bottom")
 
@@ -22,9 +22,9 @@ ggsave("./figures/tree_diameter_by_borough_histogram.png", tree_histogram_boroug
 # plot histogram of tree sizes, colored by health
 tree_histogram_health <- tree_data %>% 
   ggplot(aes(x = tree_dbh, colour = health)) + 
-  geom_freqpoly(binwidth = 1, linewidth = 1) + 
+  geom_density(linewidth = 1) + 
   labs(title = "Distribution of tree diameters by health", 
-       x = "Tree diameter (inches)", y = "Count", colour = "Health") + 
+       x = "Tree diameter (inches)", y = "Density", colour = "Health") + 
   scale_colour_discrete(type = c("red", "lightgreen", "forestgreen",  "orange")) +
   theme_bw() + 
   theme(legend.position = "bottom")
